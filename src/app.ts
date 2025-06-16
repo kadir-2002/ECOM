69ec568f9773072e27ab92a83dce0f1373a0a549
Kadir Janaka <kadirjanaka02@gmail.com>
2025-06-16 15:20:51 +0530

    "added cors"

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import path from 'path';
 import guestRoutes from './routes/guest.route';
 import authRoutes from './routes/auth.route';
@@ -22,6 +23,7 @@ dotenv.config();
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
