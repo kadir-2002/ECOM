d3b5a25280d68bed6cebc0e17495517fcfd49654
Kadir Janaka <kadirjanaka02@gmail.com>
2025-06-16 15:07:35 +0530

    "cors removed"

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import path from 'path';
 import guestRoutes from './routes/guest.route';
 import authRoutes from './routes/auth.route';
@@ -23,7 +22,6 @@ dotenv.config();
 const app = express();
 
 app.use(helmet());
-app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
